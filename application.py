456f4100c60d4bc0e2daa4f8007530deaab10dad
anirud <anirudsen@gmail.com>
2019-04-23 19:24:32 +0530

    change final 20

--- application.py
+++ application.py
@@ -26,3 +26,21 @@ def getAssetByID():
     cursor.execute("SELECT @@version;") 
     row = cursor.fetchone()
     return 'I am working'
+
+
+@app.route('/asset01')
+def getAssetByID():
+    mssql_host = 'tcp:mdpsqldbserverdev.database.windows.net'
+    mssql_db = 'mdpappdb'
+    mssql_user = 'mdpadmin'
+    mssql_pwd = 'Robo#2010'
+    mssql_port = 1433 
+    mssql_driver = 'ODBC Driver 17 for SQL Server'
+    database_server_name = "mdpsqldbserverdev"
+    dns = 'testodbc'
+
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+mssql_host+';DATABASE='+mssql_db+';UID='+mssql_user+';PWD='+ mssql_pwd)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT @@version;") 
+    row = cursor.fetchone()
+    return row[0]
