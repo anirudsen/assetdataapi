f2809ade8809191098a771feed4f3919eedd86ac
anirud <anirudsen@gmail.com>
2019-04-24 19:25:04 +0530

    chage 2200

--- application.py
+++ application.py
@@ -54,7 +54,7 @@ def getAssetByAssets():
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+mssql_host+';DATABASE='+mssql_db+';UID='+mssql_user+';PWD='+ mssql_pwd)
     cursor = cnxn.cursor()
     date_time_obj = datetime.datetime.strptime(dateval, '%Y-%m-%d')
-    cursor.execute("SELECT * FROM dbo.Device_Data where Last_Update_Date = %s.format(dateval);", )
+    cursor.execute("SELECT * FROM dbo.Device_Data where Last_Update_Date = %s.format(dateval);" )
     rows = cursor.fetchone()
     objects_list = []
     for row in rows:
