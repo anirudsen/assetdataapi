be9adbdcf1e99579afeccfd029c44e455b5c7eab
anirud <anirudsen@gmail.com>
2019-04-25 19:08:12 +0530

    comit logic 0010

--- application.py
+++ application.py
@@ -17,8 +17,8 @@ def getwelcomeMsg():
 def getData():
     mssql_host = 'tcp:mdpsqldbserverdev.database.windows.net'
     mssql_db = 'mdpappdb'
-    mssql_user = 'appadmin'
-    mssql_pwd = 'Robo#2010#'
+    mssql_user = 'mdpadmin'
+    mssql_pwd = 'Robo#2010'
     mssql_port = 1433 
     mssql_driver = 'ODBC Driver 17 for SQL Server'
     database_server_name = "mdpsqldbserverdev"
